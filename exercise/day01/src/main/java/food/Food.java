ea8b78c44bce20200d4bc934c728e85d2dd33a9e
GiuliaBortone <giulia.bortone@mail.polimi.it>
2023-12-11 17:47:21 +0100

    refactoring of day1

--- exercise/day01/src/main/java/food/Food.java
+++ exercise/day01/src/main/java/food/Food.java
@@ -8,12 +8,20 @@ public record Food(LocalDate expirationDate,
                    Boolean approvedForConsumption,
                    UUID inspectorId) {
     public boolean isEdible(Supplier<LocalDate> now) {
-        if (this.expirationDate.isAfter(now.get()) &&
-                this.approvedForConsumption &&
-                this.inspectorId != null) {
-            return true;
-        } else {
-            return false;
-        }
+        return isExpirationDateAfter(now) &&
+                isApprovedForConsumption() &&
+                hasBeenInspectedFromValidInspector();
+    }
+
+    private boolean hasBeenInspectedFromValidInspector() {
+        return this.inspectorId != null;
+    }
+
+    private Boolean isApprovedForConsumption() {
+        return this.approvedForConsumption;
+    }
+
+    private boolean isExpirationDateAfter(Supplier<LocalDate> now) {
+        return this.expirationDate.isAfter(now.get());
     }
 }

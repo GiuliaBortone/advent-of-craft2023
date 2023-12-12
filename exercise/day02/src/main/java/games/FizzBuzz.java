479b2ed576aa57efc70bef4b856462b9cb65b32f
GiuliaBortone <giulia.bortone@mail.polimi.it>
2023-12-12 10:09:03 +0100

    refactoring of day2

--- exercise/day02/src/main/java/games/FizzBuzz.java
+++ exercise/day02/src/main/java/games/FizzBuzz.java
@@ -3,25 +3,30 @@ package games;
 public class FizzBuzz {
     private FizzBuzz() {
     }
-    
+
     public static String convert(Integer input) throws OutOfRangeException {
-        if (input > 0) {
-            if (input <= 100) {
-                if (input % 3 == 0 && input % 5 == 0) {
-                    return "FizzBuzz";
-                }
-                if (input % 3 == 0) {
-                    return "Fizz";
-                }
-                if (input % 5 == 0) {
-                    return "Buzz";
-                }
-                return input.toString();
-            } else {
-                throw new OutOfRangeException();
-            }
-        } else {
+        if (input <= 0 || input > 100) {
             throw new OutOfRangeException();
         }
+
+        return convertToString(input);
+    }
+
+    private static String convertToString(Integer input) {
+        if (isDivisibleBy(input, 3) && isDivisibleBy(input, 5)) {
+            return "FizzBuzz";
+        }
+        if (isDivisibleBy(input, 3)) {
+            return "Fizz";
+        }
+        if (isDivisibleBy(input, 5)) {
+            return "Buzz";
+        }
+        return input.toString();
+    }
+
+    private static boolean isDivisibleBy(Integer input, int divisor) {
+        return input % divisor == 0;
     }
 }
+
